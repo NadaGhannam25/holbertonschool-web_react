eeffce79636cda6d45f5cd3336a1733057190b95
root <Nada.Ghannam19@gmail.com>
2026-04-14 16:26:46 +0000

    Add TypeScript task_0 student interface and config files

--- TypeScript/task_0/.eslintrc.js
+++ TypeScript/task_0/.eslintrc.js
@@ -1,19 +1,13 @@
-module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-  },
-  extends: [
-    'airbnb-typescript/base',
+module.exports =  {
+  parser:  '@typescript-eslint/parser',
+  extends:  [
+    'plugin:@typescript-eslint/recommended',
   ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: './tsconfig.json',
-    sourceType: 'module',
+  parserOptions:  {
+    ecmaVersion:  2020,
+    sourceType:  'module',
+    project: './tsconfig.json'
   },
-  plugins: [
-    '@typescript-eslint',
-  ],
-  rules: {
+  rules:  {
   },
-};
+}
